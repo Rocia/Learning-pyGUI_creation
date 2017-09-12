4aa0f1a67d402e3d7b9ac7605f318ce30bd1588f
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-09-12 09:09:12 +0530

    update

--- Tkinter/tkcanvas.py
+++ Tkinter/tkcanvas.py
@@ -5,7 +5,10 @@ C = tkinter.Canvas(top, bg="black", height=250, width=300)
 
 coord = 10, 50, 240, 210
 arc = C.create_arc(coord, start=0, extent=90, fill="green")
-
 C.pack()
 
+coordp = 10, 50, 240, 210
+D = tkinter.Canvas(top, bg="black", height=250, width=300)
+oval = D.create_polygon(coord,fill="yellow" )
+D.pack()
 top.mainloop()
