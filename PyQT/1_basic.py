36c193cf84645091fb394e35e403e2cfa5570b3f
Rocia <rocia.fernandes@gmail.com>
2018-02-13 18:33:52 +0530

    PyQT GUI creation

---
+++ PyQT/1_basic.py
@@ -0,0 +1,5 @@
+import qtpy
+import sys
+from qtpy import QtGui
+
+app = QtGui.API(sys.argv)
---
+++ PyQT/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 18:32:47 2018
+
+@author: root
+"""
+
+import sys
+from PyQt4 import QtGui
