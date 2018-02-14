a239dc1fc9ca852d49c5ac9842ce77a603df3fc3
Rocia <rocia.fernandes@gmail.com>
2018-02-14 15:52:43 +0530

    Quit option on window

---
+++ PyQT/5_Quit.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
+from PyQt5.QtGui import QIcon
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):               
+        
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)       
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Quit button')    
+        self.setWindowIcon(QIcon('images/LLOYtech.png'))        
+        self.show()
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
