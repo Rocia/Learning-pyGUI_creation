c6bf70c2d9e9ea5e5d0f689fb5bec7a223df77b5
Rocia <rocia.fernandes@gmail.com>
2018-02-14 16:18:49 +0530

    Labels and their formatting

---
+++ PyQT/15_Labels.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication
+from PyQt5.QtGui import QIcon
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        
+        lbl1 = QLabel('LLOY technology', self)
+        lbl1.move(15, 10)
+
+        lbl2 = QLabel('solutions', self)
+        lbl2.move(35, 40)
+        
+        lbl3 = QLabel('for programmers', self)
+        lbl3.move(55, 70)        
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('LLOY TECH')    
+        self.setWindowIcon(QIcon('images/LLOYtech.png'))
+        self.show()
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
