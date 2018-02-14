914426c213922b4b9ae4df18ca118eba79690200
Rocia <rocia.fernandes@gmail.com>
2018-02-14 12:10:03 +0530

    Simple window

--- PyQT/1_basic.py
+++ PyQT/1_basic.py
@@ -1,5 +1,17 @@
-import qtpy
-import sys
-from qtpy import QtGui
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-app = QtGui.API(sys.argv)
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)  #resizes window to dimensions specified in the parameters
+    w.move(300, 300)    #move the window to the position on the screen specified by these co-ordinates
+    w.setWindowTitle('Simple App')  #Title of the window
+    w.show()
+    
+    sys.exit(app.exec_())
